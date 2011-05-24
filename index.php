4f78ebf9992edba46b85bc632809ca8fb684bb0f
Craig Blagg <craig.blagg@storyworldwide.com>
2011-05-24 17:59:00 +0100

    Scrolling backgrounds
    Signed-off-by: Craig Blagg <craig.blagg@storyworldwide.com>

--- index.php
+++ index.php
@@ -1,198 +1,487 @@
-<!DOCTYPE html> 
-<html> 
-<head> 
-<title>Westfield Stratford</title> 
-<script type="text/javascript" src="js/jquery-1.5.2.min.js"></script> 
-<script type="text/javascript" src="js/jquery-ui-1.8.8.custom.min.js"></script>  
-<script type="text/javascript" src="js/jquery.ui.multidraggable-1.8.8.js"></script>  
-<link type="text/css" rel="stylesheet" href="css/style.css" />
 
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+<title>Westfield Stratford</title>
+<script type="text/javascript" src="js/jquery-1.js"></script>
+<script type="text/javascript" src="js/jquery-ui-1.js"></script>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/jquery.fullbg.min.js"></script>
+<link type="text/css" rel="stylesheet" href="css/style.css">
+<style type="text/css">
+html{overflow: hidden;}
+
+</style>
+<!-- clock - home page only -->
+<style type="text/css">
+@import "css/jquery.countdown.css";
+#countdown { width: 240px; height: 25px; }
+</style>
+
+<script type="text/javascript" src="js/jquery.countdown.js"></script>
 <script type="text/javascript">
-$(document).ready(function(){
-	visibleBool = false;
-	$('.module').addClass('draggable');	
-	$('#slideContent').css({'left':window.innerWidth});
-	$(function() {
-		$(".draggable").multidraggable();
-	});
-	
-	$('#button').click( function(){
-			if(visibleBool == true){
-			for (n=1;n<=numberElements;n++){
-			var currentLeft = parseFloat(($('#' + n).css('left').split("px"))[0]);
-			$('#' + n).animate({'left': currentLeft + window.innerWidth},500);
-			}
-			$('#slideContent').animate({'left': window.innerWidth}, 500, function() {$('#slideContent').css({'display':'none'});});
-			visibleBool = false;
-
-		}
-		else{
-			for (n=1;n<=numberElements;n++){
-			var currentLeft = parseFloat(($('#' + n).css('left').split("px"))[0]);
-			$('#' + n).animate({'left': currentLeft - window.innerWidth},500);
-			}
-			$('#slideContent').load('ajaxContent/content1.html');
-			$('#slideContent').css({'display':'block','left':window.innerWidth});
-			//alert(($('#slideContent').css('width').split("px"))[0]);
-			$('#slideContent').animate({'left': (window.innerWidth - ($('#slideContent').css('width').split("px"))[0]) / 2}, 500);
-
-			visibleBool = true;
-
-		}
-		
-
-
-	})
-	
-	var numberElements = $('#content').children().length;
-	var numberNavElements = $('#navigation').children().length;
-	var navWidth = (window.innerWidth - 150)/numberNavElements;
-	//alert(navWidth);
-	
-	document.body.onresize = function(){
-		navWidth = (window.innerWidth - 150)/6;
-		for (n=1;n<=6;n++){
-			$('#nav' + n).css({'width':navWidth});
-		}
-	};
-	for (n=1;n<=numberNavElements;n++)
-	{
-		$('#nav'+ n).css({'width':navWidth});
-	}
-	var availableWidth = window.innerWidth - (numberNavElements * navWidth) - 30;
-	var depth = numberElements;
-	var i=0;
-	var left=0;
-	var top=0;
-	var rightMargin = 0;
-	var goUnderBool = false;
-	var width=$('#wrapper').css('width').split("px");
-	for (i=0;i<=numberElements;i++)
-	{
-		left = Math.floor(Math.random() * width[0]);
-		top = Math.floor(Math.random() * window.innerHeight);
-		if (left + parseFloat($('#'+i).css('width')) >= parseFloat(width[0])){
-			left = width - $('#'+i).css('width');
-		}
-		if (top + parseFloat($('#'+i).css('height')) >= window.innerHeight){
-			top = window.innerHeight - $('#'+i).css('height');
-		}
-		$('#' + i).css({'left':left,'top':top})
-	}
-	
-	for (n=1;n<=numberNavElements;n++)
-	{
-		availableWidth = availableWidth - rightMargin;
-		
-		var offset = Math.floor(Math.random() * 2);
-		if (offset == 0 || n == 1){
-			rightMargin = Math.floor(Math.random() * availableWidth);
-		}
-		
-		var topMargin = parseFloat(($('#nav' + n).css('height').split("px"))[0]) + parseFloat(($('#nav' + n).css('marginTop').split("px"))[0]);
-		
-		if (offset == 1 && n > 1 && goUnderBool == false){
-			$('#nav' + n).css({'marginTop':topMargin});
-			
-			var goUnder = Math.floor(Math.random() * 2);
-			if (goUnder == 1 && goUnderBool == false && n > 1){
-				var leftMargin = parseFloat(($('#nav' + n).css('width').split("px"))[0]);
-				newLeftMargin = leftMargin * -1 - rightMargin;
-				$('#nav' + n).css({'marginLeft':newLeftMargin, 'marginRight':rightMargin});
-			}
-			goUnderBool = true;
-
-		}	
-		else{
-			goUnderBool = false;
-			rightMargin = Math.floor(Math.random() * availableWidth);
-			$('#nav' + n).css({'marginRight':rightMargin});
-		}
-	}
-	
-	
-	$('.draggable').mousedown(function(e){
-		$('#' + e.target.id).css({'z-index':depth});
-		depth = depth + 1;
-	});
+$(function () {
+	var austDay = new Date(2011, 5, 1, 9, 0, 0, 0);
+	$('#countdown').countdown({until: austDay});
+	$('#year').text(austDay.getFullYear());
 });
 </script>
 
+
+<script type="text/javascript">
+
+
+$(document).ready(function(){
+	visibleBool = true;
+		
+	var numberElements = $('.dialog').length;
+
+	var centre = {
+		width: window.innerWidth/2,
+		height: window.innerHeight / 2
+	}	
+	
+	var width = 1280;
+	var height = 950;
+	
+	var widthRatio = window.innerWidth/width;
+	var heightRatio = window.innerHeight/height;
+	
+	var maxRatio = 1.2;
+	if (widthRatio >= maxRatio){
+		widthRatio = maxRatio;
+	}
+	if (heightRatio >= maxRatio){
+		heightRatio = maxRatio;
+	}
+	
+	var sideBorders;
+	    
+    $(window).resize(function(){
+    sideBorders = (window.innerWidth - parseFloat($('#wrapper').css('width').split("px")[0]))/2;
+    $('.sides').css({'width':sideBorders});
+    
+    for (n=1;n<=numberBGS;n++){
+        	$('#background' + n).css({'height': parseFloat($('#background1').css('height').split("px")[0])});
+    		if (n < activeBG){
+    		    $('#background' + n).css({'top': (parseFloat($('#background1').css('height').split("px")[0]) * (n - activeBG)) + "px"});
+    		}
+    		if (n == activeBG){
+    		}
+    		if (n > activeBG){
+    		    $('#background' + n).css({'top': ((parseFloat($('#background' + activeBG).css('top').split("px")[0]) + parseFloat($('#background' + activeBG).css('height').split("px")[0])) * (n - activeBG)) + "px"});
+    		}
+
+	}
+    centre.width = window.innerWidth/2;
+    centre.height = window.innerHeight/2;
+    widthRatio = window.innerWidth/width;
+	heightRatio = window.innerHeight/height;
+	if (widthRatio >= maxRatio){
+		widthRatio = maxRatio;
+	}
+	if (heightRatio >= maxRatio){
+		heightRatio = maxRatio;
+	}
+    
+    for (n=1;n<=numberElements;n++){
+		$('#dialog' + n).parent().css({'left':centre.width - (parseFloat($('#dialog' + n).parent().css('width').split("px")[0])/2) + (offsets[n].x * widthRatio), 'top':centre.height - (parseFloat($('#dialog' + n).parent().css('height').split("px")[0])/2) + (offsets[n].y * heightRatio)});
+		}
+    
+    });
+    
+	$('#button').click( function(){	
+	var fadeTime = 500;
+			if(visibleBool == true){
+				//$('.dialog').parent().fadeOut(500);
+				for (n=1;n<=numberElements;n++){
+					$('#dialog' + n).parent().fadeOut(fadeTime);
+				}
+				$('#slideContent').load('ajaxContent/content' + activeBG + '.html');
+				$('#slideContent').css({'display':'block','left':window.innerWidth}).delay(fadeTime);
+				$('#slideContent').animate({'left': (window.innerWidth - ($('#slideContent').css('width').split("px"))[0]) / 2}, fadeTime).delay(fadeTime);
+				$('#button').html('<p>close me</p>');
+				visibleBool = false;
+			}
+			else{
+				//$('.dialog').parent().fadeIn(500);
+				for (n=1;n<=numberElements;n++){
+					$('#dialog' + n).parent().delay(fadeTime).fadeIn(fadeTime);
+				}
+				$('#slideContent').animate({'left': window.innerWidth}, fadeTime, function() {$('#slideContent').css({'display':'none'});});
+				$('#button').html('<p>open me</p>');
+				visibleBool = true;
+			}
+	});
+	
+	var numberBGS = $('.bg_img').length;
+	var activeBG = 1;		
+		
+	$('#up').click( function(){
+	if(activeBG < numberBGS){
+	for (n=1;n<=numberBGS;n++){
+		$('#background' + n).animate({'top':parseFloat($('#background' + n).css('top').split("px")[0]) - parseFloat($('#background' + activeBG).css('height').split("px")[0]) + "px"}, 1000)
+		//alert($('#background' + n).css('top'));
+	}
+	activeBG = activeBG + 1;
+	$('#background' + (activeBG - 1)).attr('src','img/bg_image' + (activeBG - 1) + '.jpg');
+	$('#background' + (activeBG + 1)).attr('src','img/bg_image' + (activeBG + 1) + '.jpg');
+	$('#background' + (activeBG - 1)).fullBg();
+	$('#background' + (activeBG + 1)).fullBg();
+	}
+	else{
+		alert('you are at the bottom');
+	}
+	})
+	
+	$('#down').click( function(){
+	if (activeBG > 1){
+	for (n=1;n<=numberBGS;n++){
+		$('#background' + n).animate({'top':parseFloat($('#background' + n).css('top').split("px")[0]) + parseFloat($('#background' + activeBG).css('height').split("px")[0]) + "px"}, 1000)
+	}
+	activeBG = activeBG - 1;
+	$('#background' + (activeBG - 1)).attr('src','img/bg_image' + (activeBG - 1) + '.jpg');
+	$('#background' + (activeBG + 1)).attr('src','img/bg_image' + (activeBG + 1) + '.jpg');
+	$('#background' + (activeBG - 1)).fullBg();
+	$('#background' + (activeBG + 1)).fullBg();
+	}
+	else{
+		alert('at top');
+	}
+	})
+
+	var offsets = [
+					// we dont use this first one to keep numbers correct
+					{
+						x: 0,
+						y: 0,
+						maxX: 0,
+						maxY: 0
+					},
+					// we do from here
+					{
+						x: -273,
+						y: -166,
+						maxX: -50,
+						maxY: 200
+					},
+					{
+						x: -245,
+						y: 203,
+						maxX: -20,
+						maxY: 100
+					},
+					{
+						x: 56,
+						y: -180,
+						maxX: 40,
+						maxY: 10
+					},
+					{
+						x: 293,
+						y: -246,
+						maxX: 80,
+						maxY: 120
+					},
+					{
+						x: 390,
+						y: -39,
+						maxX: 200,
+						maxY: -50
+					},
+					{
+						x: 383,
+						y: 261,
+						maxX: 100,
+						maxY: -80
+					},
+					{
+						x: 78,
+						y: 325,
+						maxX: 300,
+						maxY: 100
+					},
+					{
+						x: 95,
+						y: 44,
+						maxX: 320,
+						maxY: -10
+					},
+					
+				]	
+	
+	$(function(){
+                // Dialog
+                
+                $('#dialog1').dialog({
+                    autoOpen: true,
+                    width: 379,
+                    height: 323,
+                    position: [0,0],
+                    resizable: false,
+					dialogClass:'dialog-class1'
+                });
+
+                $('#dialog2').dialog({
+                    autoOpen: true,
+                    width: 378,
+                    height: 304,
+                    position: [0,0],
+                    resizable: false,
+					dialogClass:'dialog-class2'
+                });
+				
+                $('#dialog3').dialog({
+                    autoOpen: true,
+                    width: 347,
+                    height: 90,
+                    position: [0,0],
+                    resizable: false,
+					dialogClass:'dialog-class3'
+                });
+                
+                $('#dialog4').dialog({
+                    autoOpen: true,
+                    width: 279,
+                    height: 149,
+                    position: [0,0],
+                    resizable: false,
+					dialogClass:'dialog-class4'
+                });
+                $('#dialog5').dialog({
+                    autoOpen: true,
+                    width: 248,
+                    height: 237,
+                    position: [0,0],
+                    resizable: true,
+					dialogClass:'dialog-class5'
+                });
+                $('#dialog6').dialog({
+                    autoOpen: true,
+                    width: 297,
+                    height: 168,
+                    position: [0,0],
+                    resizable: false,
+					dialogClass:'dialog-class6'
+                });
+                $('#dialog7').dialog({
+                    autoOpen: true,
+                    width: 288,
+                    height: 213,
+                    position: [0,0],
+                    resizable: false,
+					dialogClass:'dialog-class7'
+                });
+                $('#dialog8').dialog({
+                    autoOpen: true,
+                    width: 328,
+                    height: 332,
+                    position: [0,0],
+                    resizable: false,
+					dialogClass:'dialog-class8'
+                });           
+                
+                for (n=1;n<=numberElements;n++){
+					$('#dialog' + n).parent().css({'left':centre.width - (parseFloat($('#dialog' + n).parent().css('width').split("px")[0])/2) + (offsets[n].x * widthRatio), 'top':centre.height - (parseFloat($('#dialog' + n).parent().css('height').split("px")[0])/2) + (offsets[n].y * heightRatio)});
+				}                
+            });	
+});
+</script>
+
+<style type="text/css">
+/*demo page css*/
+#dialog_link {
+	padding: 0;
+	text-decoration: none;
+	position: relative;
+}
+
+
+.ui-dialog {
+	border:1px solid black;
+}
+
+#dialog1 {
+	background:red;
+}
+#dialog2 {
+	background:gainsboro;
+}
+#dialog3 {
+	background:#000;
+}
+#dialog4 {
+	background:#6fcdf4;
+}
+#dialog5 {
+	background:gainsboro;
+}
+#dialog6 {
+	background:gainsboro;
+}
+#dialog7 {
+	background:gainsboro;
+}
+#dialog8 {
+	background:gainsboro;
+}
+
+/*title bar*/
+</style>
+<link type="text/css" href="css/ui-lightness/jquery-ui-1.8.13.custom.css" rel="stylesheet" />
+
 </head>
 <body>
-<div id="navigation">
-	<li id="nav1">
-		<a href="#">Nav 1</a>
-		<ul>
-			<li><a href="#">Sub nav</a></li>
-			<li><a href="#">Sub nav</a></li>
-		</ul>
-	</li>
-	<li id="nav2">
-		<a href="#">Navigation 2</a>
-		<ul>
-			<li><a href="#">Sub nav</a></li>
-			<li><a href="#">Sub nav</a></li>
-			<li><a href="#">Sub nav</a></li>
-		</ul>
-	</li>
-	<li id="nav3">
-		<a href="#">Nav 3</a>
-		<ul>
-			<li><a href="#">Sub nav</a></li>
-			<li><a href="#">Sub nav</a></li>
-			<li><a href="#">Sub nav</a></li>
-			<li><a href="#">Sub nav</a></li>
-			<li><a href="#">Sub nav</a></li>
-			<li><a href="#">Sub nav</a></li>
-		</ul>
-	</li>
-	<li id="nav4">
-		<a href="#">Nav 4</a>
-		<ul>
-			<li><a href="#">Sub nav</a></li>
-			<li><a href="#">Sub nav</a></li>
-			<li><a href="#">Sub nav</a></li>
-		</ul>
-	</li>
-	<li id="nav5">
-		<a href="#">Nav 5</a>
-		<ul>
-			<li><a href="#">Sub nav</a></li>
-			<li><a href="#">Sub nav</a></li>
-			<li><a href="#">Sub nav</a></li>
-		</ul>
-	</li>
-	<li id="nav6">
-		<a href="#">Nav 6</a>
-		<ul>
-			<li><a href="#">Sub nav</a></li>
-			<li><a href="#">Sub nav</a></li>
-		</ul>
-	</li>
-</div>
+<nav class="clearfix">
+	<ul id="navigation">
+		<li id="nav1"> <a href="#"> Home </a>
+			<ul>
+				<li> <a href="#"> Sub nav </a> </li>
 
-<div class="clearfix"></div>
+				<li> <a href="#"> Sub nav </a> </li>
+			</ul>
+		</li>
+		<li id="nav2"> <a href="#"> What's Here </a>
+			<ul>
 
-<div id="wrapper">
-<div id="content">
-	<div id="1" class="module"></div>
-	<div id="2" class="module"></div>
-	<div id="3" class="module"></div>
-	<div id="4" class="module"></div>
-	<div id="5" class="module"></div>
-	<div id="6" class="module"></div>
-	<div id="7" class="module"></div>
-	<div id="8" class="module"></div>
-	<div id="9" class="module"></div>
-	<div id="10" class="module"></div>
-	<div id="11" class="module"></div>
-	<div id="12" class="module"></div>
-	<div id="13" class="module"></div>
-	<div id="14" class="module"></div>
+				<li> <a href="#"> Sub nav </a> </li>
+				<li> <a href="#"> Sub nav </a> </li>
+				<li> <a href="#"> Sub nav </a> </li>
+
+			</ul>
+		</li>
+		<li id="nav3"> <a href="#"> Celebrating Reinvention </a>
+			<ul>
+				<li> <a href="#"> Sub nav </a> </li>
+
+				<li> <a href="#"> Sub nav </a> </li>
+				<li> <a href="#"> Sub nav </a> </li>
+				<li> <a href="#"> Sub nav </a> </li>
+
+				<li> <a href="#"> Sub nav </a> </li>
+				<li> <a href="#"> Sub nav </a> </li>
+			</ul>
+		</li>
+		<li id="nav4"> <a href="#"> Explore the Area </a>
+
+			<ul>
+				<li> <a href="#"> Sub nav </a> </li>
+				<li> <a href="#"> Sub nav </a> </li>
+				<li> <a href="#"> Sub nav </a> </li>
+
+			</ul>
+		</li>
+		<li id="nav5"> <a href="#"> Get the List </a>
+			<ul>
+				<li> <a href="#"> Sub nav </a> </li>
+
+				<li> <a href="#"> Sub nav </a> </li>
+				<li> <a href="#"> Sub nav </a> </li>
+			</ul>
+		</li>
+	</ul>
+
+</nav>
+<div id="maincontent">
+	<div id="wrapper">
+		<div id="content">
+			<div id="dialog1" class="dialog" title="d1">
+				<img src="fashion.jpg">
+			</div>
+			<!-- /dialog -->
+			<div id="dialog2" class="dialog" title="d2">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+			</div>
+			<!-- /dialog -->
+			<div id="dialog3" class="dialog" title="d3">
+				<div id="countdown">
+					here ...
+				</div>
+			</div>
+			<!-- /dialog -->
+			<div id="dialog4" class="dialog" title="d4">
+
+				<div id="countdown">
+					here ...
+				</div>
+			</div>
+			<!-- /dialog -->
+			<div id="dialog5" class="dialog" title="d5">
+				<div id="countdown">
+					here ...
+				</div>
+			</div>
+
+			<!-- /dialog -->
+			<div id="dialog6" class="dialog" title="d6">
+				<div id="countdown">
+					here ...
+				</div>
+			</div>
+			<!-- /dialog -->
+			<div id="dialog7" class="dialog" title="d7">
+				<div id="countdown">
+
+					here ...
+				</div>
+			</div>
+			<!-- /dialog -->
+			<div id="dialog8" class="dialog" title="d8">
+				<div id="countdown">
+					here ...
+				</div>
+			</div>
+			<!-- /dialog -->
+
+		</div>
+			<!-- /content -->
+	</div>
+			<!-- /wrapper -->
+	<img src="img/bg_image1.jpg" alt="" id="background1" class="bg_img"/>
+<img src="" alt="" id="background2" class="bg_img"/>
+<img src="" alt="" id="background3" class="bg_img"/>
+<img src="" alt="" id="background4" class="bg_img"/>
+<img src="" alt="" id="background5" class="bg_img"/>		
+		
+	<div id="slideContent"></div>
+			<!-- /slideContent -->
+	<div id="up">
+		<p>up</p>
+	</div>
+	<div id="down">
+		<p>down</p>
+	</div>
+	
+	<div id="button">
+		<p>open me</p>
+	</div>
+
+			<!-- /button -->
+			
+
 </div>
-<div id="slideContent"></div>
-<div id="button"></div>
-</div>
+<footer>
+	<p>This is the footer area</p>
+</footer>
+			<!-- /footer -->
+<script type="text/javascript">
+$(window).load(function() {
+	//$("#background1").fullBg();		
+	var numberBGS = $('.bg_img').length;
+	for (n=1;n<=numberBGS;n++){
+			$('#background' + n).css({'top':(parseFloat($('#background1').css('height').split("px")[0]) * (n - 1)) + "px"});
+			$("#background" + n).fullBg();
+			
+			$('#background' + n).css({'height': parseFloat($('#background1').css('height').split("px")[0])});
+
+	}
+
+	$('#background2').attr('src','img/bg_image2.jpg');
+
+});
+
+</script>
 </body>
 </html>
